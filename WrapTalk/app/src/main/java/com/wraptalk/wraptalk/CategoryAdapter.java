7a22283d13e855a1faa51f255852309a9f30111e
JiyoungPark <realplay23@naver.com>
2015-10-23 10:08:44 +0900

    CategoryFragment 관련 File

---
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/CategoryAdapter.java
@@ -0,0 +1,30 @@
+package com.wraptalk.wraptalk;
+
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+/**
+ * Created by jiyoungpark on 15. 10. 21..
+ */
+public class CategoryAdapter extends BaseAdapter {
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
---
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/CategoryData.java
@@ -0,0 +1,9 @@
+package com.wraptalk.wraptalk;
+
+/**
+ * Created by jiyoungpark on 15. 10. 21..
+ */
+public class CategoryData {
+
+    String category;
+}
