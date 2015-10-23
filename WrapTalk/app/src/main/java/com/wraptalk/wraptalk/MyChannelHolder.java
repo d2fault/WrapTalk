df8650e56bed0d68772555790c8886533dee6f9f
JiyoungPark <realplay23@naver.com>
2015-10-23 21:06:02 +0900

    MyChannelFragment에 Temp Data 추가

--- WrapTalk/app/src/main/java/com/wraptalk/wraptalk/MyChannelHolder.java
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/MyChannelHolder.java
@@ -1,7 +1,15 @@
 package com.wraptalk.wraptalk;
 
+import android.widget.ImageView;
+import android.widget.TextView;
+
 /**
  * Created by jiyoungpark on 15. 10. 23..
  */
 public class MyChannelHolder {
+
+    ImageView imageView_gameIcon;
+    TextView textView_channelTtitle;
+    TextView textView_myNickname;
+    TextView textView_countUnreadMessage;
 }
