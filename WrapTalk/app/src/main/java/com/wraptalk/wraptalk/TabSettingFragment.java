0bd8688d5898fdbb99005e75fdf4e3d023310c80
JiyoungPark <realplay23@naver.com>
2015-10-24 14:41:35 +0900

    Setting Fragment 관련 layout 일부 수정 및 임시값 대입

--- WrapTalk/app/src/main/java/com/wraptalk/wraptalk/TabSettingFragment.java
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/TabSettingFragment.java
@@ -1,11 +1,14 @@
 package com.wraptalk.wraptalk;
 
 
-import android.os.Bundle;
 import android.app.Fragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 
 /**
@@ -14,15 +17,46 @@ import android.view.ViewGroup;
 public class TabSettingFragment extends android.support.v4.app.Fragment {
 
 
-    public TabSettingFragment() {
-        // Required empty public constructor
-    }
-
+    View view;
+    ArrayList<SettingData> source;
+    SettingAdapter customAdapter = null;
+    ListView listView_result;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        view = inflater.inflate(R.layout.fragment_tab_setting, container, false);
+
+        initModel();
+        initController();
+        initView();
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tab_setting, container, false);
+        return view;
     }
+
+    private void initModel() {
+        source = new ArrayList<>();
+        listView_result  = (ListView) view.findViewById(R.id.listVeiw_setting);
+    }
+
+    private void initView() {
+        SettingData data = new SettingData();
+
+        data.setting = "setting1";
+        source.add(data);
+
+        customAdapter.notifyDataSetChanged();
+    }
+
+    private void initController() {
+        customAdapter = new SettingAdapter(getActivity().getApplicationContext(), source);
+        listView_result.setAdapter(customAdapter);
+    }
+
+    public TabSettingFragment() {
+        // Required empty public constructor
+    }
+
 }
