2908b2d6e8b195c315c2119d8c080eda2f1dafcd
JiyoungPark <realplay23@naver.com>
2015-11-01 17:21:57 +0900

    Splash Activity 추가

---
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/SplashActivity.java
@@ -0,0 +1,23 @@
+package com.wraptalk.wraptalk;
+
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler hd = new Handler();
+
+        hd.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                finish();
+            }
+        }, 3000);
+    }
+}
