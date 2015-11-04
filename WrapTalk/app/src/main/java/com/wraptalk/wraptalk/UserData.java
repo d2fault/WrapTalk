14d447f381f96d7c26be052f2623eceb1460c3b4
JiyoungPark <realplay23@naver.com>
2015-11-04 11:23:24 +0900

    UserData를 Singleton으로 생성

--- WrapTalk/app/src/main/java/com/wraptalk/wraptalk/UserData.java
+++ WrapTalk/app/src/main/java/com/wraptalk/wraptalk/UserData.java
@@ -1,73 +1,21 @@
 package com.wraptalk.wraptalk;
 
-import android.os.Parcel;
-import android.os.Parcelable;
-
 /**
  * Created by jiyoungpark on 15. 11. 1..
  */
-public class UserData implements Parcelable {
+public class UserData {
 
     String email;
     String deviceId;
     String gcmKey;
+    String token;
 
-    protected UserData(Parcel in) {
-        email = in.readString();
-        deviceId = in.readString();
-        gcmKey = in.readString();
+    static UserData g_userData = new UserData();
+
+    private UserData() {
     }
 
-    public static final Creator<UserData> CREATOR = new Creator<UserData>() {
-        @Override
-        public UserData createFromParcel(Parcel in) {
-            return new UserData(in);
-        }
-
-        @Override
-        public UserData[] newArray(int size) {
-            return new UserData[size];
-        }
-    };
-
-    public UserData() {
-
+    public static UserData getInstance() {
+        return g_userData;
     }
-
-    @Override
-    public int describeContents() {
-        return 0;
-    }
-
-    @Override
-    public void writeToParcel(Parcel dest, int flags) {
-        dest.writeString(email);
-        dest.writeString(deviceId);
-        dest.writeString(gcmKey);
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getDeviceId() {
-        return deviceId;
-    }
-
-    public void setDeviceId(String deviceId) {
-        this.deviceId = deviceId;
-    }
-
-    public String getGcmKey() {
-        return gcmKey;
-    }
-
-    public void setGcmKey(String gcmKey) {
-        this.gcmKey = gcmKey;
-    }
-
 }
